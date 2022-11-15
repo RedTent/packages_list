317374e3d3ab426a0281a48b0914b261079e958c
RedTent <30325995+RedTent@users.noreply.github.com>
2022-11-15 10:47:21 +0100

    packages_list  eerste versie

---
+++ packages_list.R
@@ -0,0 +1,89 @@
+install.packages("pak")
+
+my_packages <- c(
+
+ "devtools",
+ "usethis",
+ "bookdown",
+ "tinytex",
+ "microbenchmark",
+ "roxygen2",
+ "testthat",
+ "shiny",
+ "htmlwidgets",
+ "openxlsx",
+ "tidyverse",
+ "fpp3",
+ "datapasta",
+ "servr",
+ "gt",
+ "pdftools",
+ "pak",
+ "webshot",
+ "covr",
+ "pkgdown",
+
+ "DT",
+ "trend",
+ "vegan",
+
+ "twn",
+ "aquodom",
+
+ # GIS
+ "sf",
+ "leaflet",
+ "leaflet.extras",
+ "tmap",
+
+ # Visualisation
+ "ggspatial",
+ "ggforce",
+ "ggtext",
+ "gganimate",
+ "ggrepel",
+ "ggbeeswarm",
+ "plotly",
+ "colorspace",
+ "RColorBrewer",
+
+ # Modelling
+ "tidymodels"
+
+)
+
+pak::pak(my_packages)
+
+pak::pak(
+        c("luisDVA/annotater", 
+          "RedTent/HHSKwkl"
+        )
+)
+
+# onderstaande packages worden vaak vanzelf al meegenomen
+niet_meer_nodig <- c(
+ # Statistics
+ "modelr",
+ "caret",
+ "e1071",
+ "glmnet",
+ "tree",
+ "randomForest",
+ "leaps",
+ "gbm",
+ "ISLR",
+ "MASS",
+ "zyp",
+ "Kendall",
+ # Overig
+ "gridExtra",
+ "packcircles",
+ "ggmap",
+ "swirl",
+ "rgdal",
+ "gstat",
+ "curl",
+ "hexbin"
+
+)
+
